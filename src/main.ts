1ed931fa39deccb95a1d31795888ab636d056530
amirkhanyan11 <artyom.amirkhanyan11@gmail.com>
2025-02-13 17:07:29 +0400

    added docker

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe());
+  const port = process.env.PORT || 3000;
+  await app.listen(port, () => console.log(`Listening on port ${port}`));
 }
 bootstrap();
